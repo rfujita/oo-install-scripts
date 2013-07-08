7cdff8ff7b22069f366032468b368737df6b6922
Ryo Fujita <rfujita@redhat.com>
2013-07-08 17:06:49 +0900

    Update broker-node-auth-setup.sh

--- broker-node-auth-setup.sh
+++ broker-node-auth-setup.sh
@@ -9,6 +9,7 @@ oo-register-dns -h ${NODENAME} -d ${DOMAIN} -n ${NODEIP} -k ${KEYFILE}
 
 # Copy over broker pub key info
 ssh root@${NODEHOSTNAME} "if [ ! -d "/root/.ssh/" ] ;then mkdir -m=700 /root/.ssh/; fi"
+ssh root@${NODEHOSTNAME} "chcon system_u:object_r:ssh_home_t:s0 /root/.ssh/"
 scp /etc/openshift/rsync_id_rsa.pub root@${NODEHOSTNAME}:/root/.ssh/
 
 # Setup broker pub key for authentication
